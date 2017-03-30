7fde9708d3221fcc48a70e70afece89b8b3fe001
Nikita Lukin <nikitadecember@mail.ru>
2017-03-30 14:18:02 +0300

    Add files via upload

---
+++ FastSort/FastSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace FastSort
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int size;
+            Console.WriteLine("Введите размер массива: ");
+            size = Convert.ToInt32(Console.ReadLine());            
+            Random rng = new Random();            
+            int[] a = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                a[i] = rng.Next(0, Int32.MaxValue);
+            }
+
+            Stopwatch swsin = new Stopwatch();
+            Stopwatch swmul = new Stopwatch();
+
+            swsin.Start();
+            QuickSort.singlequickSort(a, 0, a.Length - 1);
+            swsin.Stop();
+
+            swmul.Start();
+            QuickSort.quickSort(a, 0, a.Length - 1);
+            swmul.Stop();
+
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(a[i]);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+            Console.WriteLine(String.Format("Время работы с потоками: {0}, без потоков: {1}", swmul.ElapsedMilliseconds, swsin.ElapsedMilliseconds));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FastSort/FastSort/QuickSort.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace FastSort
+{
+    class QuickSort
+    {
+        public class Wrapper
+        {
+            private int left;
+            private int right;
+            private int[] mass;
+            public Wrapper(int[] _mass, int _left, int _right)
+            {
+                this.left = _left;
+                this.right = _right;
+                this.mass = _mass;
+            }
+            public void TestMethod()
+            {
+                quickSort(mass, left, right);
+            }
+        }
+        public static void quickSort(int[] a, int l, int r)
+        {
+            int p = a[(r - l) / 2 + l];
+            int temp;
+            int i = l, j = r;
+            while (i <= j)
+            {
+                while (a[i] < p && i <= r) ++i;
+                while (a[j] > p && j >= l) --j;
+                if (i <= j)
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                    ++i; --j;
+                }
+            }            
+
+            if (i < r)
+            {
+                Wrapper data = new Wrapper(a, i, r);
+                Thread myThread = new Thread(data.TestMethod);
+                myThread.Start();
+                myThread.Join();
+            }
+            if (l < j)
+            {
+                Wrapper data = new Wrapper(a, l, j);
+                Thread myThread = new Thread(data.TestMethod);
+                myThread.Start();
+                myThread.Join();
+            }
+        }
+
+        public static void singlequickSort(int[] a, int l, int r)
+        {
+            int temp;
+            int x = a[l + (r - l) / 2];           
+            int i = l;
+            int j = r;           
+            while (i <= j)
+            {
+                while (a[i] < x) i++;
+                while (a[j] > x) j--;
+                if (i <= j)
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if (i < r)
+                quickSort(a, i, r);
+
+            if (l < j)
+                quickSort(a, l, j);
+        }
+    }
+}
